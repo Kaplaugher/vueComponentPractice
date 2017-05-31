86663eb86dbdf6b6c2572924addc0c33e1f64c33
Kaplaugher <kaplaugher94@gmail.com>
2017-05-31 11:32:32 -0400

    updates

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,18 +1,30 @@
 <template>
     <div>
-        <p>A wonderful quote</p>
-
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc;"><slot name="subtitle">The subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
+    export default {
 
+    }
 </script>
 <style scoped>
     div {
         border: 1px solid #ccc;
         box-shadow: 1px 1px 2px black;
         padding: 30px;
-        margin: auto;
+        margin: 30px auto;
+        text-align: center;
+    }
+    .title {
+        font-style: italic;
     }
 
 </style>
